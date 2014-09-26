d5100014f39ccfeb6c1eb32c8ed6150e2c807d23
koen Huybrechts <koen@appsaloon.be>
2014-09-26 21:24:13 +0200

    Reorganize files Add Gulp

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,4 @@
-var search = require('./controller/search');
+var search = require('./controllers/search');
 module.exports = function(app, passport) {
   // show the home page (will also have our login links)
   app.get('/', function(req, res) {
