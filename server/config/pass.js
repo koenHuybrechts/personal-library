dcfc69c9a97c15a30eb72c56773f5b1f9570890b
koen Huybrechts <koen@appsaloon.be>
2014-10-08 16:56:11 +0200

    Implement easy authentication

--- server/config/pass.js
+++ server/config/pass.js
@@ -3,7 +3,25 @@ var passport = require('passport')
   , User = require('./../models/user');
 
 
-passport.use(new LocalStrategy(User.authenticate()));
+passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+// Simple route middleware to ensure user is authenticated.  Otherwise send to login page.
+exports.ensureAuthenticated = function ensureAuthenticated(req, res, next) {
+  console.log('in functie');
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/auth');
+};
+
+
+// Check for admin middleware, this is unrelated to passport.js
+// You can delete this if you use different method to check for admins or don't need admins
+exports.ensureAdmin = function ensureAdmin(req, res, next) {
+  console.log(req.user);
+  if(req.user && req.user.admin === true)
+    next();
+  else
+    res.send(403);
+};
--- server/routes.js
+++ server/routes.js
@@ -4,30 +4,23 @@ var pass = require('./config/pass'),
   auth = require('./controllers/auth');
 
 module.exports = function(app, passport) {
-  // show the home page (will also have our login links)
-  app.get('/', function(req, res) {
-    console.log(req.user);
 
-    res.sendFile('index.html', {root: '.build'});
+  app.get('/app', pass.ensureAuthenticated, function(req, res) {
+    res.sendFile('app.html', {root: '.build'});
   });
 
   app.get('/auth', function(req, res) {
     res.sendFile('login.html', {root: '.build'});
   });
 
-  app.get('/loginSuccess' , function(req, res, next){
-    res.send('Successfully authenticated');
-  });
-
   app.post('/auth', passport.authenticate('local'), function(req, res) {
-    res.redirect('/account');
+    res.redirect('/app');
   });
-
 
   app.get('/register' , auth.register.view);
   app.post('/register', auth.register.process);
 
-  app.get('/account', function(req, res, next) {
+  app.get('/account', pass.ensureAuthenticated, function(req, res, next) {
     console.log(req.user);
     res.send('Authenticated');
   });
